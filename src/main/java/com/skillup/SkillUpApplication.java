d6f1aaa3905ce45b5f175ee5e86532dc913616a6
raphaelzeng <skillup.raphael@gmail.com>
2022-12-15 15:44:28 -0800

    init skillup application

---
+++ src/main/java/com/skillup/SkillUpApplication.java
@@ -0,0 +1,11 @@
+package com.skillup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillUpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillUpApplication.class, args);
+    }
+}
